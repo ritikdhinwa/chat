d2efd7bab25b1af48316dd6084c4eff9547a4f17
ritikdhinwa <130830028+ritikdhinwa@users.noreply.github.com>
2023-05-08 16:16:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="chat-box">
+        <div className="User-name">
+          <p className="chat-name">Hello</p>
+        </div>
+        <div className="user-chat">
+          <div className="chats">
+            <p>hdsxjas</p>
+          </div>
+        </div>
+        <div className="message-box">
+          <form>
+            <input type="textarea" className="message"></input>
+          </form>
+        </div>
+      </div>
+      <div className="user-box">
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
